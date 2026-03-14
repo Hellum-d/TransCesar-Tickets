e7e03d7a30e3e8168db9acc7112bc67392ef8c67
Cesar <caugustorivero@unicesar.edu.co>
2026-03-14 09:29:16 -0500

    feat: crear clase Ruta e integrar pruebas en el Main

--- TransCesar/src/transcesar/Main.java
+++ TransCesar/src/transcesar/Main.java
@@ -1,14 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package transcesar;
 
-import transcesar.view.menuPrincipal;
+import transcesar.model.*;
+import java.util.ArrayList;
 
 public class Main {
-    public static void main (String [] args){
-    menuPrincipal menu = new menuPrincipal();
-    menu.mostrarMenu();
+    public static void main(String[] args) {
+        System.out.println("=== SISTEMA TRANSCESAR: PRUEBA DE RUTAS DETALLADAS ===");
+
+        Ruta rutaNorte = new Ruta("RT01", "Valledupar", "Barranquilla", 230.5, 240);
+        Ruta rutaSur = new Ruta("RT02", "Valledupar", "Bogotá", 850.0, 960);
+
+ 
+        Vehiculo bus1 = new Bus("UXX-123", rutaNorte);
+        Vehiculo mBus1 = new MicroBus("VYY-456", rutaSur);
+        Vehiculo buseta1 = new Buseta("WZZ-789", rutaNorte);
+
+        bus1.imprimirDetalle();
+        System.out.println();
+        
+        mBus1.imprimirDetalle();
+        System.out.println();
+        
+        buseta1.imprimirDetalle();
+
+        System.out.println("======================================================");
+        System.out.println("¡Prueba de Integración Exitosa!");
     }
 }
