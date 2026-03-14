d738f5414bc9ec5f0ad85ffce3a6f307755dd090
Cesar <caugustorivero@unicesar.edu.co>
2026-03-14 09:20:46 -0500

    feat: crear clase Ruta con atributos detallados

---
+++ TransCesar/src/transcesar/model/Ruta.java
@@ -0,0 +1,39 @@
+package transcesar.model;
+
+public class Ruta {
+    private String codigo;
+    private String origen;
+    private String destino;
+    private double distanciaKm;
+    private int tiempoMinutos;
+
+    public Ruta(String codigo, String origen, String destino, double distanciaKm, int tiempoMinutos) {
+        this.codigo = codigo;
+        this.origen = origen;
+        this.destino = destino;
+        this.distanciaKm = distanciaKm;
+        this.tiempoMinutos = tiempoMinutos; 
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public double getDistanciaKm() {
+        return distanciaKm;
+    }
+
+    public int getTiempoMinutos() {
+        return tiempoMinutos;
+    }
+    
+    
+}
