4ec76e933b3b20a62065c17a82200184a8fe7f83
Santiago <sbautistav@unicesar.edu.co>
2026-03-16 18:55:19 -0500

    feat: agregar fechaCompra a clase Ticket

--- TransCesar/src/transcesar/model/Ticket.java
+++ TransCesar/src/transcesar/model/Ticket.java
@@ -1,32 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package transcesar.model;
 
-public class Ticket {
+import java.time.LocalDate;
+
+public class Ticket implements Calculable, Imprimible {
 
     private Vehiculo vehiculo;
     private Pasajero pasajero;
     private double tarifaBase;
+    private LocalDate fechaCompra;
 
-    public Ticket(Vehiculo vehiculo, Pasajero pasajero, double tarifaBase){
+    public Ticket(Vehiculo vehiculo, Pasajero pasajero, double tarifaBase) {
         this.vehiculo = vehiculo;
         this.pasajero = pasajero;
         this.tarifaBase = tarifaBase;
+        this.fechaCompra = LocalDate.now();
     }
 
-    public Vehiculo getVehiculo(){
+    public Vehiculo getVehiculo() {
         return vehiculo;
     }
 
-    public Pasajero getPasajero(){
+    public Pasajero getPasajero() {
         return pasajero;
     }
 
-    public double calcularTotal(){
+    public LocalDate getFechaCompra() {
+        return fechaCompra;
+    }
+
+    public double getTarifaBase() {
+        return tarifaBase;
+    }
+
+    @Override
+    public double calcularTotal() {
         double descuento = pasajero.calcularDescuento();
         return tarifaBase - (tarifaBase * descuento);
     }
 
+    @Override
+    public void imprimirDetalle() {
+        System.out.println("=== TICKET ===");
+        System.out.println("Pasajero: " + pasajero.getNombre());
+        System.out.println("Vehículo: " + vehiculo.getPlaca());
+        System.out.println("Fecha: " + fechaCompra);
+        System.out.println("Valor: $" + calcularTotal());
+    }
 }
