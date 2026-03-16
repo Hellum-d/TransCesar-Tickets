7e142752fd8356e12a6c17fe25592db9772a4b2d
Moises <micarrillo@unicesar.edu.co>
2026-03-16 01:39:37 -0500

    implementacion de estadisticas en ticketService

--- TransCesar/src/transcesar/service/TicketService.java
+++ TransCesar/src/transcesar/service/TicketService.java
@@ -13,26 +13,79 @@ public class TicketService {
 
     private TicketDAO ticketDAO = new TicketDAO();
 
-    public boolean venderTicket(Vehiculo vehiculo, Pasajero pasajero, double precioBase){
+    public boolean venderTicket(Vehiculo vehiculo, Pasajero pasajero, double tarifaBase){
 
-       
         if(vehiculo.getPasajerosActuales() >= vehiculo.getCapacidadMaxima()){
             System.out.println("No hay cupos disponibles");
             return false;
         }
 
-    
-        Ticket ticket = new Ticket(vehiculo, pasajero, precioBase);
+        Ticket ticket = new Ticket(vehiculo, pasajero, tarifaBase);
 
-        
         ticketDAO.guardar(ticket);
 
-        
         vehiculo.setPasajerosActuales(vehiculo.getPasajerosActuales() + 1);
 
         System.out.println("Ticket vendido correctamente");
 
         return true;
+    }
+
+    public double calcularRecaudoTotal(){
+
+        double total = 0;
+
+        for(Ticket ticket : ticketDAO.listar()){
+            total += ticket.calcularTotal();
+        }
+
+        return total;
+    }
+
+    public void contarPasajerosPorTipo(){
+
+        int regulares = 0;
+        int estudiantes = 0;
+        int adultos = 0;
+
+        for(Ticket ticket : ticketDAO.listar()){
+
+            String tipo = ticket.getPasajero().getClass().getSimpleName();
+
+            if(tipo.equals("PasajeroRegular")){
+                regulares++;
+            }
+
+            if(tipo.equals("PasajeroEstudiante")){
+                estudiantes++;
+            }
+
+            if(tipo.equals("PasajeroAdultoMayor")){
+                adultos++;
+            }
+        }
+
+        System.out.println("Pasajeros regulares: " + regulares);
+        System.out.println("Pasajeros estudiantes: " + estudiantes);
+        System.out.println("Pasajeros adulto mayor: " + adultos);
+    }
+
+    public Vehiculo vehiculoMasUsado(){
+
+        Vehiculo vehiculoTop = null;
+        int max = 0;
+
+        for(Ticket ticket : ticketDAO.listar()){
+
+            Vehiculo v = ticket.getVehiculo();
+
+            if(v.getPasajerosActuales() > max){
+                max = v.getPasajerosActuales();
+                vehiculoTop = v;
+            }
+        }
+
+        return vehiculoTop;
     }
 
 }
