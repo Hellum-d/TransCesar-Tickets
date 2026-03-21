a0d6102bc0f7749881e3bdc44e8112a1c8740f7a
Santiago <sbautistav@unicesar.edu.co>
2026-03-21 01:40:06 -0500

    fix: corregir Main.java para arrancar el sistema con menuPrincipal

--- TransCesar/src/transcesar/Main.java
+++ TransCesar/src/transcesar/Main.java
@@ -1,29 +1,10 @@
 package transcesar;
 
-import transcesar.model.*;
-import java.util.ArrayList; 
+import transcesar.view.menuPrincipal;
 
-public class Main { 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("=== SISTEMA TRANSCESAR: PRUEBA DE RUTAS DETALLADAS ===");
-
-        Ruta rutaNorte = new Ruta("RT01", "Valledupar", "Barranquilla", 230.5, 240);
-        Ruta rutaSur = new Ruta("RT02", "Valledupar", "Bogota", 850.0, 960);
-
- 
-        Vehiculo bus1 = new Bus("UXX-123", rutaNorte);
-        Vehiculo mBus1 = new MicroBus("VYY-456", rutaSur);
-        Vehiculo buseta1 = new Buseta("WZZ-789", rutaNorte);
-
-        bus1.imprimirDetalle();
-        System.out.println();
-        
-        mBus1.imprimirDetalle();
-        System.out.println();
-        
-        buseta1.imprimirDetalle();
-
-        System.out.println("======================================================");
-        System.out.println("Prueba de Integracion Exitosa!");
+        menuPrincipal menu = new menuPrincipal();
+        menu.mostrarMenu();
     }
 }
